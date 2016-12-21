704eb7a85f0a040861e5b86aed470af79e5070d4
liyang-live <13526285513@163.com>
2016-12-21 11:41:06 +0800

    完善日志方法

---
+++ MicroNet/Log/EventId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MicroNet.Log
+{
+    public struct EventId
+    {
+        public int Id { get; }
+        public string Name { get; }
+
+        public override string ToString()
+        {
+            return string.Format("[Id：{0} Name:{1}]", this.Id, this.Name);
+        }
+    }
+}
---
+++ MicroNet/Log/LogLevel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MicroNet.Log
+{
+    /// <summary>
+    /// 定义日志级别
+    /// </summary>
+    public enum LogLevel
+    {
+        //
+        // 摘要:
+        // 不记录任何信息
+        None = 0,
+
+        //
+        // 摘要:
+        //关于可能影响设备功能的情况 [例如高可用性 (HA) 状态更改 ]的消息
+        Trace = 1,
+
+        //
+        // 摘要:
+        // 调试跟踪。
+        Debug = 2,
+
+        //
+        // 摘要:
+        //     信息性消息。
+        Information = 4,
+
+        //
+        // 摘要:
+        // 非关键性问题。
+        Warning = 8,
+
+        //
+        // 摘要:
+        // 可恢复的错误。
+        Error = 16,
+
+        //
+        // 摘要:
+        //     错误或应用程序崩溃
+        Critical = 256
+    }
+}
