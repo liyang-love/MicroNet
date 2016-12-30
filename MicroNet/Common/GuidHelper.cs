62947551e3cf8b2877632698aa6a28cdee6b0c4f
liyang-live <13526285513@163.com>
2016-12-30 16:02:35 +0800

    1、增加GUID帮助类 2、增加常用判断字符是否为空帮助类

---
+++ MicroNet/Common/GuidHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MicroNet.Common
+{
+    /// <summary>
+    /// 获取Guid帮助类
+    /// </summary>
+    public sealed class GuidHelper
+    {
+        /// <summary>
+        /// 获取新的Guid 返回Guid类型
+        /// </summary>
+        public static Guid GetGuid()
+        {
+            return Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// 获取Guid 返回string类型
+        /// 返回结果为全部大写字符
+        /// </summary>
+        /// <returns></returns>
+        public static string GetGuidToUpper()
+        {
+            return Guid.NewGuid().ToString().ToUpper();
+        }
+
+        /// <summary>
+        /// 获取Guid  返回不带有 “-”的Guid  
+        /// 返回结果为全部大写字符
+        /// </summary>
+        /// <returns></returns>
+        public static string GetNGuidToUpper()
+        {
+            return Guid.NewGuid().ToString("N").ToUpper();
+        }
+
+        /// <summary>
+        /// 获取Guid 返回string类型
+        /// 返回结果为全部小写字符
+        /// </summary>
+        /// <returns></returns>
+        public static string GetGuidToLower()
+        {
+            return Guid.NewGuid().ToString().ToLower();
+        }
+
+        /// <summary>
+        /// 获取Guid  返回不带有 “-”的Guid 
+        /// 返回结果为全部小写字符
+        /// </summary>
+        /// <returns></returns>
+        public static string GetNGuidToLower()
+        {
+            return Guid.NewGuid().ToString("N").ToUpper();
+        }
+    }
+}
---
+++ MicroNet/Common/StringHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MicroNet.Common
+{
+    /// <summary>
+    /// 字符串帮助类
+    /// </summary>
+    public static class StringHelper
+    {
+
+        /// <summary>
+        /// Null转成""  非空则移除里面的空白
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string NullToString(object obj)
+        {
+            if (obj == null)
+                return string.Empty;
+            else
+            {
+                return obj.ToString().Trim();
+            }
+        }
+
+    }
+}
