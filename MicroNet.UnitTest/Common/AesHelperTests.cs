b5ea61fbea177936406c4eb5603ac5d1e8a42389
liyang-live <13526285513@163.com>
2017-02-17 12:36:45 +0800

    1、新增Aes方法的单元测试

---
+++ MicroNet.UnitTest/Common/AesHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MicroNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroNet.Common.Tests
+{
+    [TestClass()]
+    public class AesHelperTests
+    {
+        [TestMethod()]
+        public void AESEncryptTest()
+        {
+           var aes= AesHelper.AESEncrypt("qwesseeed", "12345678123456781234567812345678", "1234567812345678");
+            Assert.IsTrue(aes.Length>0);
+        }
+
+        [TestMethod()]
+        public void AESDecryptTest()
+        {
+            var Aes =Convert.ToBase64String( AesHelper.AESEncrypt("qwesseeed", "12345678123456781234567812345678", "1234567812345678"));
+            var DeAes = AesHelper.AESDecrypt(Aes, "12345678123456781234567812345678", "1234567812345678");
+            Assert.IsTrue(DeAes.Length>0);
+        }
+    }
+}
---
+++ MicroNet.UnitTest/ConfigHelper/ConfigManageHelperTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MicroNet.ConfigHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroNet.ConfigHelper.Tests
+{
+    [TestClass()]
+    public class ConfigManageHelperTests
+    {
+        [TestMethod()]
+        public void ConfigStringTest()
+        {
+           // Assert.();
+        }
+
+        [TestMethod()]
+        public void ConfigIntegerTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConfigListTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConfigListIntTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConfigBooleanTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetConfigsListTest()
+        {
+           // Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateConfigrueTest()
+        {
+           // Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddServiceProviderTest()
+        {
+           // Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            //Assert.Fail();
+        }
+    }
+}
